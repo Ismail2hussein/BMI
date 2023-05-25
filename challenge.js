b9d4bf2922442ba41cd96841fe60702db5108520
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-05-25 16:30:36 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,26 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function calculateBMI(weight, height) {
+    // Calculate the BMI
+    const bmi = weight / (height ** 2);
+    
+    // Return the BMI
+    return bmi;
+  }
+  
+  // Calculate the BMI of Ali
+  const aliWeight = 70;
+  const aliHeight = 1.8;
+  const aliBMI = calculateBMI(aliWeight, aliHeight);
+  
+  // Calculate the BMI of Mohamed
+  const mohamedWeight = 80;
+  const mohamedHeight = 1.75;
+  const mohamedBMI = calculateBMI(mohamedWeight, mohamedHeight);
+  
+  // Compare the BMIs and log the result
+  if (aliBMI > mohamedBMI) {
+    console.log("Ali's BMI is higher than Mohamed's");
+  } else if (mohamedBMI > aliBMI) {
+    console.log("Mohamed's BMI is higher than Ali's");
+  } else {
+    console.log("Ali and Mohamed have the same BMI");
+  }
